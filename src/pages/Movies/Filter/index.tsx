20fb7cf3314890ae1d4f1e54dfb48025c6c54e3a
Marina Merini Valcanaia <mmvalcanaia@gmail.com>
2022-08-08 20:41:00 +0200

    Select css

--- src/pages/Movies/Filter/index.tsx
+++ src/pages/Movies/Filter/index.tsx
@@ -1,17 +1,37 @@
 import "./styles.css";
 
-import Select from "react-select";
+import Select, { StylesConfig} from "react-select";
+import { useEffect, useState } from "react";
+import { requestBackend } from "../../../util/requests";
+import { Genre } from "../../../types/genre";
+import { colors } from "react-select/dist/declarations/src/theme";
 
 const Filter = () => {
-  const options = [
-    { value: "chocolate", label: "Chocolate" },
-    { value: "strawberry", label: "Strawberry" },
-    { value: "vanilla", label: "Vanilla" },
-  ];
+  const [selectGenres, setSelectedGenres] = useState<Genre[]>([]);
+
+  useEffect(() => {
+    requestBackend({ url: "/genres", withCredentials: true }).then((response) => {
+      setSelectedGenres(response.data);
+    });
+  }, []);
+
+  const customStyles : StylesConfig<Genre> = {
+    option: (provided, state) => ({
+      ...provided,
+      background: state.isSelected ? '#312f2f' : '#6c6c6c',
+      height: '50px',
+    })
+  }
 
   return (
     <form className="form-filter">
-      <Select options={options} classNamePrefix="form-filter-select" />
+      <Select
+        options={selectGenres}
+        getOptionLabel={(genre: Genre) => genre.name}
+        getOptionValue={(genre: Genre) => String(genre.id)}
+        classNamePrefix="form-filter-select"
+        styles={customStyles}
+      />
     </form>
   );
 };
